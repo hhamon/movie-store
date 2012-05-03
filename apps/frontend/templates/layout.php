5c611658da0226b4ca5472e891433da3bb9ada16
Hugo Hamon <hugo.hamon@sensio.com>
2012-05-03 09:42:34 +0200

    [web] added web assets.

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -11,14 +11,60 @@
     <?php include_javascripts() ?>
   </head>
   <body>
-    <h1>Demo Application</h1>
+    <div id="wrapper">
+      <div id="inner">
+        <div id="header">
+          <h1>Online Movie Store</h1>
+            <div id="nav">
+              <a href="http://www.dreamtemplatestudio.com">Home</a> | <a href="http://www.dreamtemplatestudio.com">Stores</a> | <a href="http://www.dreamtemplatestudio.com">New Movies</a> | <a href="http://www.dreamtemplatestudio.com">your account</a> | <a href="http://www.dreamtemplatestudio.com">view cart</a> | <a href="http://www.dreamtemplatestudio.com">help</a>
+              </div><!-- end nav -->
+              <img src="/images/banner.jpg" width="744" height="200" alt="" />
+            </div><!-- end header -->
+            <dl id="browse">
+              <dt>Movies Category Lists</dt>
+              <dd class="first"><a href="http://www.dreamtemplatestudio.com">Action &amp; Adventure</a></dd>
+              <dd><a href="http://www.dreamtemplatestudio.com">Anime &amp; Manga</a></dd>
+              <dd><a href="http://www.dreamtemplatestudio.com">Art House &amp; International</a></dd>
+              <dd><a href="http://www.dreamtemplatestudio.com">Classics</a></dd>
+              <dd><a href="http://www.dreamtemplatestudio.com">Comedy</a></dd>
+              <dd><a href="http://www.dreamtemplatestudio.com">Cult Movies</a></dd>
+              <dd><a href="http://www.dreamtemplatestudio.com">Drama</a></dd>
+              <dd><a href="http://www.dreamtemplatestudio.com">New &amp; Future Releases</a></dd>
+              <dd><a href="http://www.dreamtemplatestudio.com">Horror</a></dd>
+              <dd><a href="http://www.dreamtemplatestudio.com">Musicals</a></dd>
+              <dd><a href="http://www.dreamtemplatestudio.com">Mystery &amp; Suspense</a></dd>
+              <dd><a href="http://www.dreamtemplatestudio.com">Science Fiction &amp; Fantasy</a></dd>
+              <dd class="last"><a href="http://www.dreamtemplatestudio.com">Westerns</a></dd>
 
-    <?php if ($sf_user->hasFlash('notice')) : ?>
-        <div class="notice">
-            <?php echo $sf_user->getFlash('notice') ?>
-        </div>
-    <?php endif ?>
+              <dt>Search </dt>
+                <dd class="searchform">
+                  <form action="?" method="get">
+                    <div><input name="q" type="text" value="Keyword" class="text" /></div>
+                    <div class="softright"><input type="image" src="/images/btn_search.gif" /></div>
+                  </form>
+                </dd>
+              </dl>
 
-    <?php echo $sf_content ?>
+              <div id="body">
+                <div class="inner">
+                  <?php if ($sf_user->hasFlash('notice')) : ?>
+                    <div class="notice">
+                      <?php echo $sf_user->getFlash('notice') ?>
+                    </div>
+                  <?php endif ?>
+
+                  <?php echo $sf_content ?>
+                </div>
+              </div><!-- end body -->
+              <div class="clear"></div>
+              <div id="footer">
+                  Copyright &copy; 2010
+                  &nbsp;
+                  <div id="footnav">
+                    <a href="http://www.dreamtemplatestudio.com">home</a> | <a href="http://www.dreamtemplatestudio.com">contact Us</a>
+                  </div><!-- end footnav -->
+              </div><!-- end footer -->
+      </div><!-- end inner -->
+    </div><!-- end wrapper -->
   </body>
-</html>
+</html>    
